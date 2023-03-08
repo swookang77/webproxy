7221f926180d019f93bd437e81ff849b9ab88cc1
Ubuntu <ubuntu@ip-172-31-6-22.ap-northeast-2.compute.internal>
2023-03-08 10:49:30 +0000

    add comment

--- tiny/tiny.c
+++ tiny/tiny.c
@@ -43,5 +43,40 @@ void doit(int fd)
 {
   int is_static;
   struct stat sbuf;
-  char buf[MAXLINE]
+  char buf[MAXLINE], method[MAXLINE], uri[MAXLINE], version[MAXLINE];
+  char filename[MAXLINE], cgiargs[MAXLINE];
+  rio_t rio; 
+
+  Rio_readinitb(&rio,fd); 
+  Rio_readlineb(&rio, buf, MAXLINE); // Rio_readlineb함수를 사용해서 요청 라인을 읽어들임
+  printf("Request headers:\n");
+  printf("%s", buf);
+  sscanf(buf, "%s %s %s", method, uri, version);
+  if (strcasecmp(method, "GET")) {
+    clienterror(fd, method, "501", "Not implemented", "Tiny does not implement this method");
+    return;
+  }
+  read_requesthdrs(&rio); //이제 다른 요청 헤더들을 무시
+
+  is_static = parse_uri(uri,filename,cgiargs);
+
+  if (stat(filename, &sbuf) < 0) { //파일이 디스크 상에 없을 경우
+    clienterror(fd, filename, "404", "Not found","Tiny couldn’t find this file");
+    return;
+  }
+  
+  if (is_static) {
+    if (!(S_ISREG(sbuf.st_mode)) || !(S_IRUSR & sbuf.st_mode)) { //보통파일인지? 이 파일을 읽을 권한을 가지고 있는지?
+      clienterror(fd, filename, "403", "Forbidden","Tiny couldn’t read the file");
+      return;
+    }
+    serve_static(fd,filename,sbuf.st_size); //정적 컨텐츠 제공
+  }
+  else {
+    if (!(S_ISREG(sbuf.st_mode)) || !(S_IXUSR & sbuf.st_mode)) { // 보통파일인지? 실행가능한지? 
+      clienterror(fd, filename, "403", "Forbidden","Tiny couldn’t run the CGI program");
+      return;
+    }
+    serve_dynamic(fd, filename, cgiargs);  //동적 컨텐츠 제공
+  }
 }
